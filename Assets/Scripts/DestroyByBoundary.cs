29528e37a0108874fb5d5fa3eba091d65d6970a1
sierraw98 <42846365+sierraw98@users.noreply.github.com>
2018-10-30 22:29:47 -0400

    2nd commit

---
+++ Assets/Scripts/DestroyByBoundary.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByBoundary : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void OnTriggerExit(Collider other)
+    {
+        Destroy(other.gameObject);
+    }
+
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject hazard;
+    public Vector3 spawnValues;
+    public int hazardCount;
+    public float spawnWait;
+    public float startWait;
+    public float waveWait;
+
+    public Text ScoreText;
+    public Text RestartText;
+    public Text GameOverText;
+
+    private bool gameOver;
+    private bool restart;
+    private int score;
+    // Use this for initialization
+    void Start ()
+    {
+        gameOver = false;
+        restart = false;
+        RestartText.text = "";
+        GameOverText.text = "";
+        score = 0;
+        UpdateScore();
+        StartCoroutine (SpawnWaves());
+	}
+    void Update()
+    {
+        if (restart)
+        {
+            if(Input.GetKeyDown (KeyCode.R))
+            {
+                UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+            }
+        }
+    
+    }
+
+    IEnumerator SpawnWaves()
+    {
+        yield return new WaitForSeconds(startWait);
+        while (true)
+        {
+            for (int i = 0; i < hazardCount; i++)
+            {
+                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate(hazard, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(spawnWait);
+            }
+            yield return new WaitForSeconds(waveWait);
+
+            if (gameOver)
+            {
+                RestartText.text = "Press 'R' for Restart";
+                restart = true;
+                break;
+         
+            }
+        }
+    }
+    public void AddScore (int newScoreValue)
+    {
+        score += newScoreValue;
+        UpdateScore();
+    }
+    void UpdateScore()
+    {
+        ScoreText.text = "Score: " + score;
+    }
+    public void GameOver()
+    {
+        GameOverText.text = "Game Over!";
+        gameOver = true;
+    }
+}
+
+
